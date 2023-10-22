928be926b518145c946735f862924e27009e6a8f
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-22 18:18:45 -0300

    add factorial

--- Client/src/testingcode/testing.js
+++ Client/src/testingcode/testing.js
@@ -6,4 +6,16 @@ const counter = () => {
 }
 
 let firstCount = counter()
-console.log(firstCount);
+console.log(firstCount)
+
+// ------------------------------------------------
+
+const factorial = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+// creo que era así jeje
