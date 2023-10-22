9ad9bf7311bbe0d135310c6a1faed2243bb16f38
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-22 19:38:53 -0300

    adding style at home

--- Client/src/Home/Home.jsx
+++ Client/src/Home/Home.jsx
@@ -1,8 +1,12 @@
+import style from './Home.module.css'
+
 const Home = () => {
   return (
-    <div>
+    <div className={style.backHome}>
+      
       <h1>Hola</h1>
       <p>¿Cómo están?, acá haciendo algo de Front</p>
+    
     </div>
 
   )
