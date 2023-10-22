98528b7d472ead6ac5db75f532539c27b0575c75
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-22 18:34:36 -0300

    add LinkedList

--- Client/src/testingcode/testing.js
+++ Client/src/testingcode/testing.js
@@ -33,3 +33,28 @@ const bubbleSort = (array) => {
 
     return array;
 }
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    add(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+}
