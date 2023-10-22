7428d4b551abb1d2c36bff13767a92c5116a7eb8
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-22 19:32:06 -0300

    style in home

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>Hola</div>;
+  return (
+    <div>
+      <h1>Hola</h1>
+      <p>¿Cómo están?, acá haciendo algo de Front</p>
+    </div>
+
+  )
+
 };
 
 export default Home;
